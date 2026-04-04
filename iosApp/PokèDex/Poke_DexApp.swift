c6d226eed425a8fdafc25cc9e9a20b85ee9c3e62
Dhararmajsinh Jadeja <drj2311@gmail.com>
2026-04-04 23:16:40 +0530

    - added xcode project

---
+++ iosApp/PokèDex/Poke_DexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poke_DexApp.swift
+//  PokèDex
+//
+//  Created by Dharamrajsinh Jadeja on 04/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Poke_DexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
